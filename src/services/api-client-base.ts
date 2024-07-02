b33b5fdeefb1cd3f6ef9fd6f9b26254483b1868a
acroak <acroak@users.noreply.github.com>
2024-07-02 11:02:24 -0400

    fetch the games from rawg api

--- src/services/api-client-base.ts
+++ src/services/api-client-base.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// remove the -base appended to the file name for customized personal API useage
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params:{
