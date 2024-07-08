6726efccd578144e78d322c2c25bb0a837ec009f
acroak <acroak@users.noreply.github.com>
2024-07-08 11:45:12 -0400

    clean up genres list styles

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner} from "@chakra-ui/react";
+import { Button, HStack, Heading, Image, List, ListItem, Spinner} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
@@ -19,17 +19,34 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     if (isLoading) return <Spinner />;
 
     return(
-
-        <List>
+        <>
+            <Heading fontSize='2xl' marginBottom={3} >Genres</Heading>       
+            <List>
             { data.map( genre => (
                 <ListItem key={ genre.id } paddingY='5px'>
                     <HStack>
-                        <Image boxSize={'32px'} borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                        <Button onClick={() => onSelectGenre(genre)} fontWeight={ genre.id == selectedGenre?.id ? 'bold' : 'normal'} fontSize='lg' variant='link'>{genre.name}</Button>
+                        <Image 
+                            boxSize={'32px'} 
+                            borderRadius={8} 
+                            objectFit='cover' 
+                            src={getCroppedImageUrl(genre.image_background)} 
+                        />
+
+                        <Button 
+                            whiteSpace='normal' 
+                            textAlign='left' 
+                            fontSize='lg' 
+                            variant='link'
+                            onClick={() => onSelectGenre(genre)} 
+                            fontWeight={ genre.id == selectedGenre?.id ? 'bold' : 'normal'} 
+                        > 
+                            { genre.name }
+                        </Button>
                     </HStack>
                 </ListItem>
             ))}
-        </List>
+            </List>
+        </>
     )
 }
 
