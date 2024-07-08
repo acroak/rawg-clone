d2cabba5c34ca9c7bb56bbf940795590aebf40b3
acroak <acroak@users.noreply.github.com>
2024-07-08 11:06:30 -0400

    sort game functionality

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -3,6 +3,10 @@
 // cropped image url: https://media.rawg.io/media/crop/600/400/games/511/5118aff5091cb3efec399c808f8c598f.jpg  
 
 const getCroppedImageUrl = (url: string) => {
+    
+    // TODO: handle properly
+    if (!url) return '';
+     
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
