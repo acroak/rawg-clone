269a55a6c5838731921a4bf412b996fc40bc79f2
acroak <acroak@users.noreply.github.com>
2024-07-08 12:10:40 -0400

    customize chakra theme

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,24 @@ const config: ThemeConfig = {
     initialColorMode: 'dark'
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({ 
+    config,
+    colors: {
+        gray: {
+            50: '#F9F9F9',
+            100: '#EDEDED',
+            200: '#D3D3D3',
+            300: '#B3B3B3',
+            400: '#A0A0A0',
+            500: '#898989',
+            600: '#636363',
+            700: '#202020',
+            800: '#121212',
+            900: '#111'
+        }
+    }
+
+
+});
 
 export default theme;
